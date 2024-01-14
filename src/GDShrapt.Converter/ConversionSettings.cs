8721e94a0e4d87d1acc218d928f7121d495626f4
Semenov Dmitry <elamaunt@gmail.com>
2024-01-15 01:47:57 +0400

    Removed converter from this repo. Bumped version

--- src/GDShrapt.Converter/ConversionSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GDShrapt.Converter
-{
-    public class ConversionSettings
-    {
-        public string Namespace { get; set; }
-        public string FileName { get; set; }
-        public bool ConvertGDScriptNamingStyleToSharp { get; set; }
-    }
-}
--- src/GDShrapt.Converter/SystemExtensionMethods.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System
-{
-    internal static class SystemExtensionMethods
-    {
-        public static bool IsNullOrEmpty(this string self) => string.IsNullOrEmpty(self);
-        public static bool IsNullOrWhiteSpace(this string self) => string.IsNullOrWhiteSpace(self);
-    }
-}
